efbf9e44c19bf613cacaae28d268ad9251d03d91
Valerio Luzzi <valerio.luzzi@gecosistema.it>
2018-08-31 15:28:50 +0200

    version 0.0.3

--- gecosistema_gdal/__init__.py
+++ gecosistema_gdal/__init__.py
@@ -1,1 +1,26 @@
-from gecosistema_gdal import *
+# -------------------------------------------------------------------------------
+# Licence:
+# Copyright (c) 2012-2018 Luzzi Valerio
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+#
+# Name:        __init__.py
+# Purpose:
+#
+# Author:      Luzzi Valerio
+#
+# Created:     31/08/2018
+# -------------------------------------------------------------------------------
+
+from .gdal_utils import *
