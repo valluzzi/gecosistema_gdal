0a20dc64b3738ab4d88a2152a3cf871cd998ea23
Valerio Luzzi <valerio.luzzi@gecosistema.it>
2019-01-17 11:56:08 +0100

    version 0.0.31

--- gecosistema_gdal/taudem.py
+++ gecosistema_gdal/taudem.py
@@ -48,6 +48,19 @@ def mpiexec(command, env={}, n=0, precond=[], postcond=[], remove=[], skipIfExis
         return postcond[0] if len(postcond) == 1 else tuple(postcond)
     return False
 
+def StreamBurning(filedem, fileriver, fileburn="", value=20, verbose=False):
+    """
+    StreamBurning
+    """
+    fileburn = fileburn if fileburn else forceext(filedem, "brn.tif")
+    env = {
+        "burn": fileburn,
+        "river": fileriver,
+        "dem": filedem,
+        "value": value
+    }
+    return gdal_numpy("""burn=np.where(river>0,dem-value,0)""", env, verbose=verbose)
+
 def PitRemove(demfile, felfile="", n=-1, skipIfExists=False, verbose=False):
     """
     PitRemove
@@ -70,3 +83,30 @@ def PitRemove(demfile, felfile="", n=-1, skipIfExists=False, verbose=False):
         SetNoData(felfile, nodata)
 
     return felfile
+
+def D8FlowDir(felfile, pfile="", sd8file="", n=-1, skipIfExists=False, verbose=False):
+    """
+    D8FlowDir
+    """
+    command = """{exe} -fel "{felfile}" -p "{pfile}" -sd8 "{sd8file}" """
+
+    pfile = remove_suffix(felfile, "fel") + "p.tif" if not pfile   else pfile
+    sd8file = remove_suffix(felfile, "fel") + "sd8.tif" if not sd8file else sd8file
+    mkdirs(justpath(pfile))
+    mkdirs(justpath(sd8file))
+
+    env = {"exe": "d8flowdir", "felfile": felfile, "pfile": pfile, "sd8file": sd8file, "n": n}
+    return mpiexec(command, env, n, precond=[felfile], postcond=[pfile, sd8file], skipIfExists=skipIfExists,
+                   verbose=verbose)
+
+def AreaD8(pfile, ad8file="", nc=True, n=-1, skipIfExists=False, verbose=False):
+    """
+    AreaD8
+    """
+    command = """{exe} -p "{pfile}" -ad8 "{ad8file}" {nc} """
+
+    ad8file = remove_suffix(pfile, "p") + "ad8.tif" if not ad8file else ad8file
+    mkdirs(justpath(ad8file))
+
+    env = {"exe": "aread8", "pfile": pfile, "ad8file": ad8file, "nc": "-nc" if nc else "", "n": n}
+    return mpiexec(command, env, n, precond=[pfile], postcond=[ad8file], skipIfExists=skipIfExists, verbose=verbose)
