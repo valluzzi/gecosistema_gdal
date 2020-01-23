9bedb9394bca0f2538d21bb98d1e3d29df1ac581
Valerio Luzzi <valerio.luzzi@gecosistema.it>
2020-01-23 18:52:28 +0100

    commit of 23/01/2020 18:52:28.40

--- gecosistema_gdal/gdal_utils.py
+++ gecosistema_gdal/gdal_utils.py
@@ -435,10 +435,12 @@ def gdal_crop(src_dataset, dst_dataset, cutline, nodata=-9999, extraparams="", v
     command += """"{src_dataset}" "{dst_dataset}" """
     command += """{extraparams}"""
 
+    filecrop = tempfname("crop",ext="tif")
+
     env = {
 
         "src_dataset": src_dataset,
-        "dst_dataset": dst_dataset,
+        "dst_dataset": filecrop,
         "xmin":xmin,
         "xmax":xmax,
         "ymin":ymin,
@@ -446,22 +448,16 @@ def gdal_crop(src_dataset, dst_dataset, cutline, nodata=-9999, extraparams="", v
         "extraparams":extraparams
     }
 
-    if Exec(command, env, precond=[src_dataset], postcond=[dst_dataset], skipIfExists=False, verbose=verbose):
-
-        print("gdal_rasterize...")
-        filemask = gdal_rasterize( cutline, dst_dataset )
-        print("filemask:", filemask)
+    if Exec(command, env, precond=[src_dataset], postcond=[filecrop], skipIfExists=False, verbose=verbose):
+        filemask = tempfname("mask",ext="tif")
+        gdal_rasterize( cutline, dst_dataset, filemask )
         if filemask:
-            print( "1) GDAL2Numpy..")
             mask, _  ,  _  = GDAL2Numpy( filemask, dtype = np.uint8 ,load_nodata_as = 0)
-            print(" 2) GDAL2Numpy..")
-            data, gt , prj = GDAL2Numpy( dst_dataset, load_nodata_as = np.nan)
+            data, gt , prj = GDAL2Numpy( filecrop, load_nodata_as = np.nan)
             data[mask == 0] = np.nan
-            print("3) Numpy2GTiff..")
-            Numpy2GTiff(data, gt, prj, forceext(filemask,"out.tif"), save_nodata_as= nodata )
-        else:
-            print("filemask does not exists!")
-
+            Numpy2GTiff(data, gt, prj, dst_dataset, save_nodata_as= nodata )
+            remove( filemask )
+        remove( filecrop)
     return dst_dataset
 
 def gdalwarp(src_dataset, dst_dataset="", cutline="", of="GTiff", nodata=-9999, xres=-1, yres=-1, interpolation="near", t_srs="",extraparams="", verbose=False):
